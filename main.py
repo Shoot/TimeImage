089b0f60c3d56893e08e9d677eb9c3aae77d29f5
Shoot <11522779+Shoot@users.noreply.github.com>
2020-01-07 16:44:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import time_image
 from waitress import serve
 
-serve(time_image.app, host='0.0.0.0', port='80')
+def main():
+	serve(time_image.app, host='0.0.0.0', port='80')
+
+if __name__ == "__main__":
+	main()
